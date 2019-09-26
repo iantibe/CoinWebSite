ab6e2a27281b76b3c1a2698f651ea35ff7b55756
Ian Tibe <imtibe@dmacc.edu>
2019-09-25 23:04:56 -0500

    added delete

---
+++ WebContent/coinlist.jsp
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+<form method = "post" action = "navigationServlet">
+<table>
+<c:forEach items="${requestScope.allItems}" var="currentitem">
+<tr>
+ <td><input type="radio" name="id" value="${currentitem.id}"></td>
+ <td>${currentitem.price}</td>
+ <td>${currentitem.type}</td>
+ </tr>
+</c:forEach>
+</table>
+<input type = "submit" value = "edit" name="doThis">
+<input type = "submit" value = "delete" name="doThis">
+<input type="submit" value = "add" name = "doThis">
+</form>
+
+
+</body>
+</html>
---
+++ src/controler/navigationServlet.java
@@ -0,0 +1,61 @@
+package controler;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.CoinItem;
+
+/**
+ * Servlet implementation class navigationServlet
+ */
+@WebServlet("/navigationServlet")
+public class navigationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public navigationServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		String act = request.getParameter("doThis");
+		if (act == null) {
+		getServletContext().getRequestDispatcher("/viewAllItemsServlet").
+		forward(request, response);
+		} else if (act.equals("delete")) {
+		
+			CoinController controller = new CoinController();	
+			int idNumber = Integer.parseInt(request.getParameter("id"));
+			CoinItem item = controller.searchById(idNumber);
+			
+			controller.delete(item);
+			getServletContext().getRequestDispatcher("/viewAllItemsServlet").forward(request, response);
+			
+		} else if (act.equals("edit")) {
+			
+		} else if (act.equals("add")) {
+		getServletContext().getRequestDispatcher("/index.html").forward(request, response);
+		}
+	}
+
+}
